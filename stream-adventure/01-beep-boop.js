864a1fc392f776776f7634211991f47b51921016
chrisbuttery <info@chrisbuttery.com>
2013-10-08 13:14:11 +1100

    Added stream-adventures

---
+++ stream-adventure/01-beep-boop.js
@@ -0,0 +1,7 @@
+/**
+ * Just to make sure everything is working, just write a `program.js` that outputs
+ * the string "beep boop" with a `console.log()`.
+ *
+ * To verify your program, run: `stream-adventure verify 01-beep-boop.js`.
+ */
+console.log('beep boop');
